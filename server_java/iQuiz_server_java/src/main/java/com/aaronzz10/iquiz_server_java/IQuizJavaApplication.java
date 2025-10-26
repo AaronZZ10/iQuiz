0b94d1333183e2f96ef42b9b350f122ab9c0b62b
Aaron Liu <aaronzz10101@gmail.com>
2025-10-26 16:45:00 -0500

    push main class

---
+++ server_java/iQuiz_server_java/src/main/java/com/aaronzz10/iquiz_server_java/IQuizJavaApplication.java
@@ -0,0 +1,11 @@
+package com.aaronzz10.iquiz_server_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IQuizJavaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(IQuizJavaApplication.class, args);
+    }
+}
