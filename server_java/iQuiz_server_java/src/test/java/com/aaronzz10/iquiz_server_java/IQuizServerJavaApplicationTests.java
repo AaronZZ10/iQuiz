6a9da16762ef5bcd300f9c148938032fc42ec073
Aaron Liu <aaronzz10101@gmail.com>
2025-10-26 16:52:35 -0500

    change server

---
+++ server_java/iQuiz_server_java/src/test/java/com/aaronzz10/iquiz_server_java/IQuizServerJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aaronzz10.iquiz_server_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IQuizServerJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
